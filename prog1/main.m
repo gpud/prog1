42624675e735efed4a30d47387a7eb8600fd2585
mubase <mubase@hannahs-iMac.local>
2015-03-15 17:48:45 +0000

    Initial Commit

---
+++ prog1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by mubase on 15/03/2015.
+//  Copyright (c) 2015 mubase. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
